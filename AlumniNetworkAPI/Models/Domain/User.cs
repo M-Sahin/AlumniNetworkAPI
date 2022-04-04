cd4a68d2e9bd1c2b33e56bfb191f5dc36e4ddeea
mehmet70 <mehmetemin.balci@live.nl>
2022-04-04 13:16:55 +0200

    user added

--- AlumniNetworkAPI/Models/Domain/User.cs
+++ AlumniNetworkAPI/Models/Domain/User.cs
@@ -2,5 +2,23 @@
 {
     public class User
     {
+        public int userId { get; set; }
+
+        // Properties
+        [Required]
+        [MaxLength(100)]
+        public string name { get; set; }
+        [Url]
+        [MaxLength(500)]
+        public string picture { get; set; }
+        [MaxLength(50)]
+        public string gender { get; set; }
+        [MaxLength(50)]
+        public string status { get; set; }
+        [MaxLength(50)]
+        public string bio { get; set; }
+        [MaxLength(50)]
+        public string fun_fact { get; set; }
+
     }
 }
