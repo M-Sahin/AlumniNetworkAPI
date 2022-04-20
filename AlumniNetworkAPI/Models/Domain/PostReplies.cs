2312c2bfbeadb44ca159b9134c4469c471fb3625
M-sahin <murat.sa25@gmail.com>
2022-04-20 10:11:54 +0200

    feat: adde dreplies

---
+++ AlumniNetworkAPI/Models/Domain/PostReplies.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlumniNetworkAPI.Models.Domain
+{
+    public class PostReplies
+    {
+        [Key]
+        public int PostReply_Id { get; set; }
+        public int Postpost_id { get; set; }
+        public Post post { get; set; }
+
+        public int Replyreply_Id { get; set; }
+        public Replies reply { get; set; }
+
+    }
+}
---
+++ AlumniNetworkAPI/Models/Domain/Replies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlumniNetworkAPI.Models.Domain
+{
+    public class Replies
+    {
+        [Key]
+        public int Reply_Id { get; set; }
+
+        [Required]
+        [MaxLength(500, ErrorMessage = "Text cant be longer than 500 characters.")]
+        public string Body { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+
+
+    }
+}
